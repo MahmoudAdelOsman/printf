4d10c1f0c8e58597a46467b907e05fbba8809ed3
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-04-25 14:21:19 -0700

    new

--- functions2.c
+++ functions2.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
-/** print pointer **/
+/** PRINT POINTER **/
 /**
- * print_pointer - prints the value of a pointer variable
+ * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
- * @buffer: buffer array to  handle print
- * @flags: calculates active flags
- * @width: width
- * @precision: precision specification
- * @size: size specifier
- * Return: Number of chars printed
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
  */
- int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size)
- {
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
 	unsigned long num_addrs;
@@ -36,6 +37,7 @@
 		num_addrs /= 16;
 		length++;
 	}
+
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
@@ -48,20 +50,21 @@
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
- }
+}
 
-/** print non printable **/
+/** PRINT NON PRINTABLE **/
 /**
- * print_non_printable - prints ascii codes in hexa of non printable cahrs
- * @types: List a of arguments
- * @buffer: buffer array to  handle print
- * @flags: calculates active flags
- * @width: width
- * @precision: precision specification
- * @size: size specifier
+ * print_non_printable - Prints ascii codes in hexa of non printable chars
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
@@ -88,18 +91,21 @@ int print_non_printable(va_list types, char buffer[], int flags, int width, int 
 
 	return (write(1, buffer, i + offset));
 }
-/** print reverse **/
+
+/** PRINT REVERSE **/
 /**
- * print_reverse - prints reverse string
- * @types: List a of arguments
- * @buffer: buffer array to  handle print
- * @flags: calculates active flags
- * @width: width
- * @precision: precision specification
- * @size: size specifier
- * Return: Number of chars printed
+ * print_reverse - Prints reverse string.
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
  */
-int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size)
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	char *str;
 	int i, count = 0;
@@ -118,7 +124,7 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
 		str = ")Null(";
 	}
 	for (i = 0; str[i]; i++)
-			;
+		;
 
 	for (i = i - 1; i >= 0; i--)
 	{
@@ -126,11 +132,10 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
 
 		write(1, &z, 1);
 		count++;
-
 	}
 	return (count);
 }
-/** print a string in rot13 **/
+/** PRINT A STRING IN ROT13 **/
 /**
  * print_rot13string - Print a string in rot13.
  * @types: Lista of arguments
@@ -163,8 +168,8 @@ int print_rot13string(va_list types, char buffer[],
 	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; in[j]; j++)
-	{
-		if (in[j] == str[i])
+		{
+			if (in[j] == str[i])
 			{
 				x = out[j];
 				write(1, &x, 1);
