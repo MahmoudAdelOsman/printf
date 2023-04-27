fe08af15e271e00efb88158db0d868019166e384
MahmoudAdelOsman <mahmoud.adel.elgandy@gmail.com>
2023-04-27 13:07:20 -0700

    update

--- get_precision.c
+++ get_precision.c
@@ -5,7 +5,6 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
@@ -39,3 +38,4 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
+/* Mahmoud Adel */
